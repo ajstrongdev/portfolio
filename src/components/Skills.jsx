3fc43a01cebbdc3fcf972c069ccc83a6387ba4f5
ajstrongdev <ajstrongdev@proton.me>
2024-09-28 19:03:38 +0100

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,7 +5,7 @@ function Skills() {
     return(
         <div className="w-full md:w-[85%] my-16 md:grid lg:grid-cols-3 md:grid-cols-2 md:gap-8 p-8 m-auto leading-normal">
             {MySkills.map((item, index) => (
-                <div className="p-8 break-words bg-white shadow-xl mb-8 rounded-lg hover:scale-105 transition-all">
+                <div className="p-8 break-words bg-white shadow-xl mb-8 rounded-lg hover:scale-105 transition-all" key={item.skill}>
                     <h1 className="text-accent-purple text-center py-4 lg:text-6xl md:text-4xl text-3xl font-bold">{item.skill}</h1>
                     <p className="text-xl">{item.description}</p>
                 </div>
